2c2ba9e9fdb92abedafd9f3d12b1abbdc01b7752
Saidkhondev <saidkhon.dev@gmail.com>
2022-03-27 15:10:15 +0500

    home page has been added

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.get("/", (req, res) => {
   res.render("Intro");
 });
 
+
 app.get("/home", (req, res) => {
   fs.readFile(db, (err, data) => {
     if (err) throw err;
@@ -22,152 +23,8 @@ app.get("/home", (req, res) => {
   });
 });
 
-app.post("/add", (req, res) => {
-  const formData = req.body;
 
-  if (formData.note.trim() === "") {
-    fs.readFile(db, (err, data) => {
-      if (err) throw err;
 
-      const notes = JSON.parse(data);
-
-      res.render("home", { error: true, notes: notes });
-    });
-  } else {
-    fs.readFile(db, (err, data) => {
-      if (err) throw err;
-
-      const notes = JSON.parse(data);
-      const note = {
-        id: id(),
-        desc: formData.note,
-        crossed: false,
-      };
-
-      notes.push(note);
-
-      fs.writeFile(db, JSON.stringify(notes), (err) => {
-        if (err) throw err;
-
-        fs.readFile(db, (err, data) => {
-          if (err) throw err;
-
-          const notes = JSON.parse(data);
-
-          res.render("home", { success: true, notes: notes });
-        });
-      });
-
-      // res.redirect("/");
-    });
-  }
-});
-
-app.post("/edit", (req, res) => {
-  const formData = req.body;
-
-  if (formData.note.trim() === "") {
-    fs.readFile(db, (err, data) => {
-      if (err) throw err;
-
-      const notes = JSON.parse(data);
-
-      res.render("home", { error: true, notes: notes });
-    });
-  } else {
-    fs.readFile(db, (err, data) => {
-      if (err) throw err;
-
-      const notes = JSON.parse(data);
-      const note = {
-        id: id(),
-        desc: formData.note,
-        crossed: false,
-      };
-
-      notes.push(note);
-
-      fs.writeFile(db, JSON.stringify(notes), (err) => {
-        if (err) throw err;
-
-        fs.readFile(db, (err, data) => {
-          if (err) throw err;
-
-          const notes = JSON.parse(data);
-
-          res.render("home", { updated: true, notes: notes });
-        });
-      });
-
-      // res.redirect("/");
-    });
-  }
-});
-
-app.get("/:id/edit", (req, res) => {
-  const id = req.params.id;
-  fs.readFile(db, (err, data) => {
-    if (err) throw err;
-
-    const notesToEdit = JSON.parse(data);
-
-    const NoteToEdit = notesToEdit.filter((e) => e.id == id)[0];
-
-    const IndexOfslicedNote = notesToEdit.indexOf(NoteToEdit);
-    const slicedNote = notesToEdit.splice(IndexOfslicedNote, 1)[0];
-
-    fs.writeFile(db, JSON.stringify(notesToEdit), (err) => {
-      if (err) throw err;
-
-      res.render("Edit", {
-        notes: notesToEdit,
-        NoteToBeEdite: slicedNote,
-        Id: id,
-      });
-    });
-  });
-});
-
-app.get("/:id/delete", (req, res) => {
-  const id = req.params.id;
-  fs.readFile(db, (err, data) => {
-    if (err) throw err;
-
-    const Notes = JSON.parse(data);
-    const filteredData = Notes.filter((e) => e.id != id);
-
-    fs.writeFile(db, JSON.stringify(filteredData), (err) => {
-      if (err) throw err;
-
-      if (filteredData.length == 0) res.redirect("/home");
-      else res.render("home", { deleted: true, notes: filteredData });
-    });
-  });
-});
-
-app.get("/:id/crossed", (req, res) => {
-  const id = req.params.id;
-  fs.readFile(db, (err, data) => {
-    if (err) throw err;
-
-    const datas = JSON.parse(data);
-
-    const noteToBeCrossed = datas.filter((e) => e.id == id)[0];
-    const indexOfNote = datas.indexOf(noteToBeCrossed);
-
-    const splicedNote = datas.splice(indexOfNote, 1)[0];
-    if (splicedNote.crossed) splicedNote.crossed = false;
-    else splicedNote.crossed = true;
-
-    datas.unshift(splicedNote);
-    fs.writeFile(db, JSON.stringify(datas), (err) => {
-      if (err) throw err;
-
-      // res.render("home", { notes: datas });
-      res.redirect("/home");
-    });
-  });
-});
 
 app.listen(PORT, () => {
   console.log("App is running on the Port : " + PORT);
